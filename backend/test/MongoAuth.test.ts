615955d05c939788ac58eb47a774ac8b870dd181
Kalanithi96 <kalanithi96@gmail.com>
2024-04-20 23:30:09 +0530

    Added test cases for MongoAuthProvider

--- backend/test/MongoAuth.test.ts
+++ backend/test/MongoAuth.test.ts
@@ -1,12 +1,16 @@
 import {describe, expect, test, beforeAll, afterAll} from '@jest/globals';
 import mongoose from "mongoose";
+import User from "./../src/auth/providers/mongo/models/user";
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDb from "./../src/db/mongo/mongodb";
+import {AuthService} from "./../src/auth/authService";
+import {MongoAuthProvider} from "./../src/auth/providers/mongo/MongoAuthProvider";
 
-describe('Connect with DB', () => {
+describe('Auth Service with Mongo Auth Provier Tests', () => {
   beforeAll(async () => {
     await connectDb('test')
+	await User.deleteMany({});
   })
 
   afterAll(async () => {
@@ -17,4 +21,32 @@ describe('Connect with DB', () => {
     const db = mongoose.connection
     expect(db.readyState).toBe(1)
   })
+
+  const authService = new AuthService(new MongoAuthProvider());
+
+  test('should not login for a non-existent user', async () => {
+	  const res = await authService.login("001","100");
+	  expect(res.statusCode).toBe(401);
+  });
+
+  test('should register a new user with unique userId', async () => {
+	  const res = await authService.register("001","100");
+	  expect(res.statusCode).toBe(201);
+  });
+
+  test('should not register a new user with duplicate userId', async () => {
+	  const res = await authService.register("001","100");
+	  expect(res.statusCode).toBe(400);
+  });
+
+  test('should login user when userId and password match', async () => {
+	  const res = await authService.login("001","100");
+	  expect(res.statusCode).toBe(200);
+  });
+
+  test('should not login when userId and password do not match', async () => {
+	  const res = await authService.login("001","101");
+	  expect(res.statusCode).toBe(401);
+  });
+
 });
